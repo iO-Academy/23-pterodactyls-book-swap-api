ff467250243fd313ba1f7ae6f81f7c6df72ef968
Natniif <fintan.hardy@gmail.com>
2023-10-02 11:26:00 +0100

    added api and controller changes

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     //
+    public function getAllBooks()
+    {
+        return Book::all();
+    }
 }
