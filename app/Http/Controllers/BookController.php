72d07e82866f5e76c5aa520104c650fc16bfad2f
Ben Peake <dearbenp@gmail.com>
2023-10-02 14:04:31 +0100

    formatting

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
-use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
@@ -12,7 +11,7 @@ class BookController extends Controller
     {
         return response()->json([
             'data' => Book::all()->makeHidden(['updated_at', 'created_at']),
-            'message' => 'success'
+            'message' => 'success',
         ]);
     }
 }
