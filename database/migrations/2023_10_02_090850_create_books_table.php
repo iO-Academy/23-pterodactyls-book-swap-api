27030d0c36033f799d71635dc31a3d6dfbc44188
Natniif <fintan.hardy@gmail.com>
2023-10-02 10:58:46 +0100

    basic table set up

---
+++ database/migrations/2023_10_02_090850_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->binary('claimed');
+            $table->integer('genre_id');
+            $table->string('page_count');
+            $table->string('claimed_by');
+            $table->string('image')->nullable();
+            $table->year('year');
+            $table->integer('review_id');
+            $table->string('email_of_owner')->nullable();
+            $table->string('name_of_owner')->nullable();
+            $table->binary('deleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
