48928fe28132a1fcfd685f80e1ce0aff82d2ce32
Ben Peake <dearbenp@gmail.com>
2023-10-02 14:10:04 +0100

    update message return

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -11,7 +11,7 @@ class BookController extends Controller
     {
         return response()->json([
             'data' => Book::all()->makeHidden(['updated_at', 'created_at']),
-            'message' => 'success',
+            'message' => 'Books successfully retrieved',
         ]);
     }
 }
