b14cf715737bcb471ac6f7d006171a7f05200ea6
James Bell <jamesbell1138@gmail.com>
2023-10-03 14:35:08 +0100

    changes to story-2

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -31,42 +31,4 @@ class BookController extends Controller
             'message' => 'Book successfully found'
         ]);
     }
-<<<<<<< HEAD
-=======
-
-    public function claimBook(int $id, Request $request)
-    {
-
-        $bookToUpdate = Book::find($id);
-
-        if ($bookToUpdate) {
-
-            if ($bookToUpdate->claimed == 1) {
-                return response()->json([
-                    'message' => "Book $id is already claimed",
-                ], 400);
-            } elseif ($bookToUpdate->claimed == 0) {
-
-                $request->validate([
-                    'claimed_by_name' => 'string|min:1|required',
-                    'email' => 'string|email|required',
-                ]);
-
-                $bookToUpdate->name = $request->name;
-                $bookToUpdate->email = $request->email;
-                $bookToUpdate->claimed = 1;
-
-                if ($bookToUpdate->save()) {
-                    return response()->json([
-                        'message' => "Book $id was claimed",
-                    ]);
-                }
-            }
-        }
-
-        return response()->json([
-            'message' => "Book $id was not found",
-        ], 404);
-    }
->>>>>>> 2c0075ad12dd03f3f22c00fb5cc90e2352f0cd15
 }
