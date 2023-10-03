e559a1e098c065b9d0bab5e547f5d388f44b177a
James Bell <jamesbell1138@gmail.com>
2023-10-03 10:07:00 +0100

    started story-2

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -11,7 +11,29 @@ class BookController extends Controller
     {
         return response()->json([
             'data' => Book::with(['genre:id,name'])->get()->makeHidden(['genre_id', 'review_id', 'deleted_at', 'deleted', 'email', 'name', 'claimed', 'year', 'page_count', 'claimed_by_name', 'updated_at', 'created_at', 'blurb']),
-            'message' => 'Book successfully retrieved',
+            'message' => 'Book successfully retrieved'
+        ]);
+    }
+
+    public function getBookFromId(int $id)
+    {
+        $book = Book::find($id);
+
+        if (!$book) {
+            return response()->json([
+                
+                "message" => "The claimed field must be a number. (and 2 more errors)",
+                "errors" =>[ 
+                    "claimed" => "The claimed field must be a number.",
+                    "genre" => "The selected genre is invalid.",
+                    "search" => "The search field must be a string." 
+                ]
+            ]);
+        }
+
+        return response()->json([
+            'data' => Book::with(['genre:id,name'])->get()->makeHidden(['genre_id', 'review_id', 'deleted_at', 'deleted', 'email', 'name', 'claimed', 'year', 'page_count', 'claimed_by_name', 'updated_at', 'created_at', 'blurb']),
+            'message' => 'Book successfully retrieved'
         ]);
     }
 }
